e81188ca81bc14b028552e60c9e46517a317df81
raffli0 <141337926+raffli0@users.noreply.github.com>
2024-06-06 19:37:53 +0700

    Update splitting.c
    optimizinggggggggg

--- Tugass C/splitting.c
+++ Tugass C/splitting.c
@@ -11,24 +11,19 @@ int main() {
     FILE *bukuFile, *jenis1File, *jenis2File, *jenis3File;
     Buku buku;
 
-    // Buka file buku.dat untuk dibaca
+    // buka file buku.dat untuk dibaca
     bukuFile = fopen("buku.dat", "rb");
     if (bukuFile == NULL) {
         printf("Gagal membuka file buku.dat untuk dibaca.\n");
         return 1;
     }
 
-    // Buka tiga file terpisah untuk jenis buku 1, 2, dan 3 untuk ditulis
+    // buka tiga file terpisah untuk jenis buku 1, 2, dan 3 untuk ditulis
     jenis1File = fopen("jenis1.dat", "wb");
     jenis2File = fopen("jenis2.dat", "wb");
     jenis3File = fopen("jenis3.dat", "wb");
-    if (jenis1File == NULL || jenis2File == NULL || jenis3File == NULL) {
-        printf("Gagal membuka file jenis1.dat, jenis2.dat, atau jenis3.dat untuk ditulis.\n");
-        fclose(bukuFile);
-        return 1;
-    }
 
-    // Baca setiap record dari file buku.dat dan tulis ke file yang sesuai berdasarkan jenis buku
+    // read semua buku sesuai jenis untuk di pisah
     while (fread(&buku, sizeof(buku), 1, bukuFile) == 1) {
         if (buku.jenis == 1) {
             fwrite(&buku, sizeof(buku), 1, jenis1File);
@@ -39,7 +34,6 @@ int main() {
         }
     }
 
-    // Tutup semua file
     fclose(bukuFile);
     fclose(jenis1File);
     fclose(jenis2File);
