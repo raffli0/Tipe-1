f74ed0f995ca5082ab2c71065844cf3a5db7e111
raffli0 <141337926+raffli0@users.noreply.github.com>
2024-06-06 19:38:17 +0700

    Update splittingOutput.c
    optimizingggggggg

--- Tugass C/splittingOutput.c
+++ Tugass C/splittingOutput.c
@@ -10,7 +10,7 @@ typedef struct {
 void tampilkanBuku(FILE *file) {
     Buku buku;
 
-    // Baca setiap record dari file dan tampilkan
+    // baca setiap record dari file dan tampilkan
     printf("Kode Buku\tJenis\tHarga Sewa\tStok\n");
     while (fread(&buku, sizeof(buku), 1, file) == 1) {
         printf("%d\t\t%d\t%.2f\t\t%d\n", buku.kode_buku, buku.jenis, buku.harga_sewa, buku.stok);
@@ -20,28 +20,20 @@ void tampilkanBuku(FILE *file) {
 int main() {
     FILE *jenis1File, *jenis2File, *jenis3File;
 
-    // Buka file jenis1.dat, jenis2.dat, dan jenis3.dat untuk dibaca
+    // buka file jenis1.dat, jenis2.dat, dan jenis3.dat untuk dibaca
     jenis1File = fopen("jenis1.dat", "rb");
     jenis2File = fopen("jenis2.dat", "rb");
     jenis3File = fopen("jenis3.dat", "rb");
-    if (jenis1File == NULL || jenis2File == NULL || jenis3File == NULL) {
-        printf("Gagal membuka file jenis1.dat, jenis2.dat, atau jenis3.dat untuk dibaca.\n");
-        return 1;
-    }
 
-    // Tampilkan isi file jenis1.dat
     printf("Isi file jenis1.dat:\n");
     tampilkanBuku(jenis1File);
 
-    // Tampilkan isi file jenis2.dat
     printf("\nIsi file jenis2.dat:\n");
     tampilkanBuku(jenis2File);
 
-    // Tampilkan isi file jenis3.dat
     printf("\nIsi file jenis3.dat:\n");
     tampilkanBuku(jenis3File);
 
-    // Tutup semua file
     fclose(jenis1File);
     fclose(jenis2File);
     fclose(jenis3File);
