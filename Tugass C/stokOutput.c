b1fa082eaa637b06abb8601bfc14a714b99ba78c
raffli0 <141337926+raffli0@users.noreply.github.com>
2024-06-06 19:39:42 +0700

    Update stokOutput.c

--- Tugass C/stokOutput.c
+++ Tugass C/stokOutput.c
@@ -10,7 +10,7 @@ typedef struct {
 void tampilkanBuku(FILE *file) {
     Buku buku;
 
-    // Baca setiap record dari file dan tampilkan
+    // baca setiap record dari file dan tampilkan
     printf("Kode Buku\tJenis\tHarga Sewa\tStok\n");
     while (fread(&buku, sizeof(buku), 1, file) == 1) {
         printf("%d\t\t%d\t%.2f\t\t%d\n", buku.kode_buku, buku.jenis, buku.harga_sewa, buku.stok);
@@ -20,23 +20,19 @@ void tampilkanBuku(FILE *file) {
 int main() {
     FILE *stokDiAtas10File, *stokDiBawah10File;
 
-    // Buka file stok_di_atas_10.dat dan stok_di_bawah_10.dat untuk dibaca
+    // buka file stok_di_atas_10.dat dan stok_di_bawah_10.dat untuk dibaca
     stokDiAtas10File = fopen("stok_di_atas_10.dat", "rb");
     stokDiBawah10File = fopen("stok_di_bawah_10.dat", "rb");
-    if (stokDiAtas10File == NULL || stokDiBawah10File == NULL) {
-        printf("Gagal membuka file stok_di_atas_10.dat atau stok_di_bawah_10.dat untuk dibaca.\n");
-        return 1;
-    }
 
-    // Tampilkan isi file stok_di_atas_10.dat
+    // tampilkan isi file stok_di_atas_10.dat
     printf("Isi file stok_di_atas_10.dat:\n");
     tampilkanBuku(stokDiAtas10File);
 
-    // Tampilkan isi file stok_di_bawah_10.dat
+    // tampilkan isi file stok_di_bawah_10.dat
     printf("\nIsi file stok_di_bawah_10.dat:\n");
     tampilkanBuku(stokDiBawah10File);
 
-    // Tutup semua file
+    // tutup semua file
     fclose(stokDiAtas10File);
     fclose(stokDiBawah10File);
 
